1c09ca50901d77393456c6d4b6895a0cddf9716c
Mariana Cid <cidmariana5@gmail.com>
2020-03-27 10:58:18 -0600

    Show page created to show student on professor show page

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,6 +1,11 @@
 class ProfessorsController < ApplicationController
 
     def index 
+        require'pry'; binding.pry
         @professors = Professor.all
+    end 
+
+    def show
+
     end 
 end 
