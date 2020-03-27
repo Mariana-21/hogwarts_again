cb6b38b43a5d236ce4165227db8ea0f94a219881
Mariana Cid <cidmariana5@gmail.com>
2020-03-27 09:36:40 -0600

    Test added to assert professor on page.

---
+++ spec/features/user_can_see_all_professors_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe "As a visitor", type: :feature do
+    it "can see all professors and their attributes in one page" do 
+        professor_1 = Professor.create(name: "Minerva McGonagall",
+                                        age: "204",
+                                        specialty: "Transfiguration")
+        professor_2 = Professor.create(name: "Rolanda Hooch",
+                                        age: "104",
+                                        specialty: "Flying")
+        professor_3 = Professor.create(name: "Filius Flitwick",
+                                        age: "304",
+                                        specialty: "Charms")
+
+        visit "/professors"
+
+        within "#professor-#{professor_1.id}" do
+            expect(page).to have_content(professor_1.name)
+            expect(page).to have_content(professor_1.age)
+            expect(page).to have_content(professor_1.specialty)
+        end
+        within "#professor-#{professor_2.id}" do 
+            expect(page).to have_content(professor_2.name) 
+            expect(page).to have_content(professor_2.age) 
+            expect(page).to have_content(professor_2.specialty) 
+        end 
+        within "#professor-#{professor_3.id}" do 
+            expect(page).to have_content(professor_3.name) 
+            expect(page).to have_content(professor_3.age) 
+            expect(page).to have_content(professor_3.specialty) 
+        end 
+    end
+end
